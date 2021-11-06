172ca3eff1099bc64235f02a8a4c4e5f462687e2
Muneeb Khan <muneebkhan7@live.com>
2021-11-06 11:40:08 -0400

    hello

---
+++ hardcoded_data.py
@@ -0,0 +1,29 @@
+departments = ["Civil",
+               "Urban",
+               "Petroleum",
+               "Earthquake",
+               "Mechanical",
+               "Textile",
+               "IM",
+               "Automotive",
+               "Electrical",
+               "CIS",
+               "Electronic",
+               "Software",
+               "BCIT",
+               "Mathematics",
+               "Chemistry",
+               "Chemical",
+               "Materials",
+               "Metallurgical",
+               "Polymer",
+               "Food",
+               "Environmental",
+               "Economics", ]
+domains = ['Promotions', 'Technical', 'Robotics', 'Social Media']
+year = ['First Year', 'Second Year', 'Third Year', 'Forth Year']
+
+
+departments_choices = [(k, k) for k in departments]
+domains_choices = [(k, k) for k in domains]
+year_choices = [(k, k) for k in year]
